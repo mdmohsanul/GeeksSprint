c6ecc733c022f7a43a3d0bc5e77450ce8b1650a3
Mohsanul786 <mdmohsan2407@gmail.com>
2025-06-05 20:54:46 +0530

    fix : axios url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
-  // baseURL: "https://geekssprint-backend.onrender.com/api/v1",
+  // baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://geekssprint-backend.onrender.com/api/v1",
 
   withCredentials: true, // Send and recieve cookies
 });

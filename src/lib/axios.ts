c2cf40eea99d1e344eadc4bd1f9b2ddfbc4bf49a
Mohsanul786 <mdmohsan2407@gmail.com>
2025-06-05 17:53:08 +0530

    prod: url added

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const api = axios.create({
   // baseURL: "http://localhost:4000/api/v1",
-  baseURL: "https://geeks-sprint-backend.vercel.app/api/v1",
+  baseURL: "https://geekssprint-backend.onrender.com/api/v1",
 
   withCredentials: true, // Send and recieve cookies
 });

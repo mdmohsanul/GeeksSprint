3e72ab8572ca8dd607a9503b008a619310c1fb7b
Mohsanul786 <mdmohsan2407@gmail.com>
2025-06-05 16:29:44 +0530

    prod: url added

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
-  // baseURL: "https://fotive-backend.vercel.app/api/v1",
+  // baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://geeks-sprint-backend.vercel.app/api/v1",
 
   withCredentials: true, // Send and recieve cookies
 });
